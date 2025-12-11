0d3fe39bcd53e45739e53aa971263d1165fc6e2e
Amr CHATELAIN <amr.chatelain@gmail.com>
2025-12-11 14:35:56 +0100

    done

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    // Ensure correct MIME types
+    assetsInlineLimit: 0,
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript; charset=UTF-8',
+    },
+  },
 })
