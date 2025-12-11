9a6153f55c78332eb597ef94731b33c26ba5f97f
Amr CHATELAIN <amr.chatelain@gmail.com>
2025-12-11 14:56:42 +0100

    fixing bug

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,2 +1,2 @@
-const API = "https://e-commerce-09wy.onrender.com";
+const API = "https://e-commerce-09wy.onrender.com" || 5005;
 export default API;
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,19 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-    // Ensure correct MIME types
-    assetsInlineLimit: 0,
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript; charset=UTF-8',
-    },
-  },
+  plugins: [react(),tailwindcss()],
 })
